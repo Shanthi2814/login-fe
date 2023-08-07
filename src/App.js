6f4f80e1e6f8471fe8edc4e4885535648bd8f348
Shanthi S <130643002+Shanthi2814@users.noreply.github.com>
2023-08-07 11:47:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-// import Signup from './Signup'
-//  import Login from './Login'
+
+
 import 'bootstrap/dist/css/bootstrap.css'
-// import { useState } from 'react';
-//  import {BrowserRouter,Routes,Route} from 'react-router-dom'
-// import Dashboard from './Dashboard';
+
 import Home from './Home';
 
 function App() {
   return (
     
-    // <BrowserRouter>
-    // <Routes>
-    // <Route path='/' element={<Home/>}/>
-    //   <Route path='/register' element={<Signup/>}/>
-    //   <Route path='/login' element={<Login/>}/>
-    //   <Route path='/dashboard' element={<Dashboard/>}/>
-    // </Routes>
-    // </BrowserRouter>
+   
     <div><Home/></div>
     
     
